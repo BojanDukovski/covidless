4d1afc0f2dc2cea85d924ef5127c579ae970407a
x2Dzi <bojan.dukovski211@gmail.com>
2022-07-01 17:41:32 +0200

    Initial commit

---
+++ covidless/src/main/java/app/covidless/CovidlessApplication.java
@@ -0,0 +1,13 @@
+package app.covidless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidlessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidlessApplication.class, args);
+    }
+
+}
